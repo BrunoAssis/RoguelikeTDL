95f07705b28ccf6609681c690096f646accc8d46
Bruno Assis <brunoassis@gmail.com>
2018-03-02 00:42:49 -0300

    Finish part 4

--- engine.py
+++ engine.py
@@ -2,7 +2,7 @@ import tdl
 
 from entity import Entity
 from input_handlers import handle_keys
-from map_utils import make_map
+from map_utils import GameMap, make_map
 from render_functions import clear_all, render_all
 
 SCREEN_WIDTH = 80
@@ -17,9 +17,15 @@ def main():
     room_min_size = 6
     max_rooms = 30
 
+    fov_algorithm = 'BASIC'
+    fov_light_walls = True
+    fov_radius = 10
+
     colors = {
         'dark_wall': (0, 0, 100),
-        'dark_ground': (50, 50, 150)
+        'dark_ground': (50, 50, 150),
+        'light_wall': (130, 110, 50),
+        'light_ground': (200, 180, 50)
     }
 
     player = Entity(int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 2), '@', (255, 255, 255))
@@ -32,14 +38,21 @@ def main():
     root_console = tdl.init(SCREEN_WIDTH, SCREEN_HEIGHT, title='Roguelike Tutorial Revised')
     con = tdl.Console(SCREEN_WIDTH, SCREEN_HEIGHT)
 
-    game_map = tdl.map.Map(map_width, map_height)
+    game_map = GameMap(map_width, map_height)
     make_map(game_map, max_rooms, room_min_size, room_max_size, map_width, map_height, player)
 
+    fov_recompute = True
+
     while not tdl.event.is_window_closed():
-        render_all(con, entities, game_map, root_console, SCREEN_WIDTH, SCREEN_HEIGHT, colors)
+        if fov_recompute:
+            game_map.compute_fov(player.x, player.y, fov=fov_algorithm, radius=fov_radius, light_walls=fov_light_walls)
+
+        render_all(con, entities, game_map, fov_recompute, root_console, SCREEN_WIDTH, SCREEN_HEIGHT, colors)
         tdl.flush()
 
         clear_all(con, entities)
+
+        fov_recompute = False
 
         for event in tdl.event.get():
             if event.type == 'KEYDOWN':
@@ -61,6 +74,7 @@ def main():
             dx, dy = move
             if game_map.walkable[player.x + dx, player.y + dy]:
                 player.move(dx, dy)
+                fov_recompute = True
 
         if exit_action:
             return True
